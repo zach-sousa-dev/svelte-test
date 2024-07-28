143eae50dd25f1897d4d78e5befbc263774d98c5
Z4CH4RYSNP <zs20np@snpsteam.com>
2024-07-27 22:55:37 -0400

    first

---
+++ ui/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+    import logo from './assets/appLogoFull.svg';
+    import Div from './lib/Div.svelte';
+
+    const songList =    [
+                            {
+                                'name': 'The Real Slim Shady',
+                                'artist': 'Eminem'
+                            },
+
+                            {
+                                'name': 'Lose Yourself',
+                                'artist': 'Eminem'
+                            },
+
+                            {
+                                'name': 'Purple Pills',
+                                'artist': 'D12'
+                            }
+                        ];
+</script>
+
+<main>
+    <img src={logo} alt="Logo for app called trackIt">
+    
+    {#if 1+1 == 2}
+        <p>1+1 does, in fact, equal 2.</p>
+    {:else}
+        <p>Else...</p>
+    {/if}
+
+    {#each songList as song}
+        <p><b>SONG:</b> {song.name} <b>ARTIST:</b> {song.artist}</p>
+    {/each}
+
+    <Div text="This is a component." />
+</main>
---
+++ ui/src/lib/Div.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let text;
+</script>
+
+<div>
+    {text}
+</div>
+
+<style>
+    div {
+        width: 50%;
+        color: white;
+        position: absolute;
+        left: 25%;
+        border: 10px solid lightblue;
+        border-radius: 10px;
+    }
+</style>
